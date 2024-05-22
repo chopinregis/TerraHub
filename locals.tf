3bf9809ad2f0f5130ac743887be38d95404e4315
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-21 23:36:01 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,8 +1,10 @@
    locals {
+
+     vm_config = yamldecode(file("${path.module}/configs/vm_config.yaml"))["vm_settings"]
+
      common_tags = {
        ManagedBy = "Terraform"
        Environment = "Dev"
      }
    }
    
-
