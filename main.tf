541266177f3964e029c6a13d58f6ea7d9d43e656
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-05-21 22:42:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,12 +35,12 @@ resource "azurerm_virtual_network" "my_vnet" {
 resource "azurerm_subnet" "my_subnet" {
   name                 = "mySubnet"
   resource_group_name  = azurerm_resource_group.my_rg.name
-  virtual_network_name = azurerm_virtual_network.my_rg.name
+  virtual_network_name = azurerm_virtual_network.my_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
+resource "azurerm_network_interface" "my_nic" {
+  name                = "myNIC"
   location            = azurerm_resource_group.my_rg.location
   resource_group_name = azurerm_resource_group.my_rg.name
 
